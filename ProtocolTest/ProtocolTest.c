4704d995442b1a19ac7ec28231c330081e69b335
smh9800 <108454450+smh9800@users.noreply.github.com>
2023-07-24 12:26:24 +0900

    7/24 오전 끝
    버튼 콜백과 라이브러리 분리

--- ProtocolTest/ProtocolTest.c
+++ ProtocolTest/ProtocolTest.c
@@ -1,10 +1,17 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 
+// 나중에 라이브러리로 분리할 FASTECH 관련 함수, 자료형
+typedef uint8_t BYTE; //메뉴얼상에서 BYTE라는 자료형을 쓰는데 일단 사용자 정의로 만들어둠 unsigned char와 unit8_t모두 같은 역할인데 둘중 하나로가야 중간중간 형변환 관련해서 이슈가 안생길듯함
+
+bool FAS_Connect(BYTE sb1, BYTE sb2, BYTE sb3, BYTE sb4, int iBdID);
+bool FAS_ConnectTCP(BYTE sb1, BYTE sb2, BYTE sb3, BYTE sb4, int iBdID);
+
 // Define the callback function prototype
-bool FAS_Connect(GtkButton *button, gpointer user_data);
+static void on_button_connect_clicked(GtkButton *button, gpointer user_data);
 
 int main(int argc, char *argv[]) {
     GtkBuilder *builder;
@@ -33,7 +40,7 @@ int main(int argc, char *argv[]) {
 
     // Connect the signal handler (callback function) with user_data as builder
     button = gtk_builder_get_object(builder, "button_connect");
-    g_signal_connect(button, "clicked", G_CALLBACK(FAS_Connect), builder);
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_connect_clicked), builder);
 
     // Start the GTK+ main loop
     gtk_main();
@@ -41,7 +48,11 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-bool FAS_Connect(GtkButton *button, gpointer user_data) {
+/************************************************************************************************************************************
+ ********************************GUI 프로그램의 버튼 등 구성요소들에서 사용하는 callback함수*************************************************
+ ************************************************************************************************************************************/
+ 
+static void on_button_connect_clicked(GtkButton *button, gpointer user_data) {
     // Get the GtkBuilder object passed as user data
     GtkBuilder *builder = GTK_BUILDER(user_data);
 
@@ -56,7 +67,7 @@ bool FAS_Connect(GtkButton *button, gpointer user_data) {
         g_print("IP: %s\n", ip_text);
 
         // Parse and store IP address in BYTE format
-        guint8 sb1, sb2, sb3, sb4;
+        BYTE sb1, sb2, sb3, sb4;
         gchar *ip_copy = g_strdup(ip_text); // 문자열 복사
         gchar *token, *saveptr;
         token = strtok_r(ip_copy, ".", &saveptr);
@@ -71,16 +82,25 @@ bool FAS_Connect(GtkButton *button, gpointer user_data) {
                     token = strtok_r(NULL, ".", &saveptr);
                     if (token != NULL) {
                         sb4 = atoi(token);
-                        // Now you have the IP address components as BYTE values (sb1, sb2, sb3, sb4)
                         g_print("Parsed IP: %d.%d.%d.%d\n", sb1, sb2, sb3, sb4);
                     }
                 }
             }
         }
-        g_free(ip_copy); // 할당된 문자열 메모리를 해제
-        return true;
+        g_free(ip_copy);
     } else {
         g_print("Please enter a valid IP.\n");
-        return false;
     }
+}
+
+/************************************************************************************************************************************
+ ********************************나중에 라이브러리로 뺄 FASTECH 라이브러리와 같은 기능의 함수*************************************************
+ ************************************************************************************************************************************/
+ 
+bool FAS_Connect(BYTE sb1, BYTE sb2, BYTE sb3, BYTE sb4, int iBdID){
+     
+}
+ 
+bool FAS_ConnectTCP(BYTE sb1, BYTE sb2, BYTE sb3, BYTE sb4, int iBdID){
+    
 }
